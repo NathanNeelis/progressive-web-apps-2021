368234998e74663d2a337d076d7c28d042ad7535
NathanNeelis <nathan.neelis@hva.nl>
2021-03-22 15:25:43 +0100

    minify javascript build script

--- scripts/build-js.js
+++ scripts/build-js.js
@@ -1,9 +1,16 @@
 const gulp = require('gulp')
 const concat = require('gulp-concat');
+const minify = require('gulp-minify');
 
 return gulp.src([
         './src/js/homeSearch.js',
         './src/js/movieSearch.js',
     ])
+    .pipe(minify({
+        noSource: true,
+        ext: {
+            min: '.js'
+        }
+    }))
     .pipe(concat('index.js'))
     .pipe(gulp.dest('./static/js'))
